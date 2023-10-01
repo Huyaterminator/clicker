c23f2865999c3e977a6cbe7c67eea3c9de04f83e
Nguyen Quoc Huy <146612626+Huyaterminator@users.noreply.github.com>
2023-10-01 15:59:24 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript code in script.js
+const signupLink = document.getElementById("signupLink");
+const signupForm = document.getElementById("signupForm");
+
+signupLink.addEventListener("click", () => {
+    signupForm.style.display = "block";
+});
