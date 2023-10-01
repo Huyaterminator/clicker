f881b64334e80ed867b7ad46eef517e5830f7135
Nguyen Quoc Huy <146612626+Huyaterminator@users.noreply.github.com>
2023-10-01 16:40:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 // JavaScript code in script.js
 const loginForm = document.getElementById("loginForm");
-const signupForm = document.getElementById("signupForm");
+const registerForm = document.getElementById("registerForm");
 
 loginForm.addEventListener("submit", function (event) {
     event.preventDefault();
     const loginUsername = document.getElementById("loginUsername").value;
     const loginPassword = document.getElementById("loginPassword").value;
-    // Implement login logic here (e.g., check if the user exists in localStorage)
+
+    // Check if the username and password match a stored user (for example, in localStorage)
+    // Implement your authentication logic here
+
+    // Example: Checking if the user exists with a hardcoded username and password
+    if (loginUsername === "user" && loginPassword === "password") {
+        alert("Login successful!");
+    } else {
+        alert("Login failed. Please check your username and password.");
+    }
 });
 
-signupForm.addEventListener("submit", function (event) {
+registerForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    const signupUsername = document.getElementById("signupUsername").value;
-    const signupPassword = document.getElementById("signupPassword").value;
-    const user = {
-        username: signupUsername,
-        password: signupPassword
+    const registerUsername = document.getElementById("registerUsername").value;
+    const registerPassword = document.getElementById("registerPassword").value;
+
+    // Store the new user's data (for example, in localStorage)
+    // Implement your user registration logic here
+
+    // Example: Storing user data in localStorage
+    const newUser = {
+        username: registerUsername,
+        password: registerPassword
     };
-    localStorage.setItem(signupUsername, JSON.stringify(user));
-    alert("Signup successful! You can now log in.");
+    localStorage.setItem(registerUsername, JSON.stringify(newUser));
+    alert("Registration successful! You can now log in.");
 });
