881720d1e79edbdb01b49ec04b35a3189ba98d2c
Nguyen Quoc Huy <146612626+Huyaterminator@users.noreply.github.com>
2023-10-01 16:25:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // JavaScript code in script.js
-const signupLink = document.getElementById("signupLink");
+const loginForm = document.getElementById("loginForm");
 const signupForm = document.getElementById("signupForm");
 
-signupLink.addEventListener("click", () => {
-    signupForm.style.display = "block";
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const loginUsername = document.getElementById("loginUsername").value;
+    const loginPassword = document.getElementById("loginPassword").value;
+    // Implement login logic here (e.g., check if the user exists in localStorage)
+});
+
+signupForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const signupUsername = document.getElementById("signupUsername").value;
+    const signupPassword = document.getElementById("signupPassword").value;
+    const user = {
+        username: signupUsername,
+        password: signupPassword
+    };
+    localStorage.setItem(signupUsername, JSON.stringify(user));
+    alert("Signup successful! You can now log in.");
 });
