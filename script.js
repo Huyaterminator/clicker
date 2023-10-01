722e986b5b0961b6e06b9137826b548116cbefa2
Nguyen Quoc Huy <146612626+Huyaterminator@users.noreply.github.com>
2023-10-01 16:06:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ const signupForm = document.getElementById("signupForm");
 signupLink.addEventListener("click", () => {
     signupForm.style.display = "block";
 });
+
+// JavaScript code in script.js
+const signupForm = document.getElementById("signupForm");
+
+signupForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from submitting the traditional way
+
+    // Get user input from the signup form
+    const newUsername = document.getElementById("newUsername").value;
+    const newPassword = document.getElementById("newPassword").value;
+
+    // Create a user object with the data
+    const user = {
+        username: newUsername,
+        password: newPassword
+    };
+
+    // Store the user object in localStorage
+    localStorage.setItem(newUsername, JSON.stringify(user));
+
+    // Optionally, you can clear the form fields
+    document.getElementById("newUsername").value = "";
+    document.getElementById("newPassword").value = "";
+
+    // Notify the user that signup was successful or redirect them to the game page
+    alert("Signup successful! You can now log in.");
+});
